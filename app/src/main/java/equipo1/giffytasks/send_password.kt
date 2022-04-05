e6ad8eae93faef3171845e7c5493003906295580
Paloma <paloma.arguello216561@potros.itson.edu.mx>
2022-04-05 02:14:34 -0500

    actualizacion de pantallas

---
+++ app/src/main/java/equipo1/giffytasks/send_password.kt
@@ -0,0 +1,11 @@
+package equipo1.giffytasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class send_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_password)
+    }
+}
