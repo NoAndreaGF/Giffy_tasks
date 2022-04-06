45f0865e2453d909f2e36422e10004e251c4cf85
ServandoValdez <servando.valdez216775@potros.itson.edu.mx>
2022-04-06 16:05:46 -0700

    algunas pantallas
    Se agregaron las pantallas 39, 40, 43,44(medio) y  43

---
+++ app/src/main/java/equipo1/giffytasks/my_group.kt
@@ -0,0 +1,11 @@
+package equipo1.giffytasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class my_group : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_group)
+    }
+}
