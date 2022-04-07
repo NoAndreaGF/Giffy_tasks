d40f774c5f00c9c2c903a8a156861af9f7efe29b
ServandoValdez <servando.valdez216775@potros.itson.edu.mx>
2022-04-06 23:56:17 -0700

    nuevas pantallas
    pantallas 50, 51, 52 y 53

---
+++ app/src/main/java/equipo1/giffytasks/quiz_gift.kt
@@ -0,0 +1,11 @@
+package equipo1.giffytasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quiz_gift : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_gift)
+    }
+}
