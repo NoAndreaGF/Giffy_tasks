a5264af60022ae0addbcc58f15246c42b6f7a4b8
andrea <andrea.garcia216626@potros.itson.edu.mx>
2022-05-12 10:28:56 -0700

    subir quiz

---
+++ app/src/main/java/equipo1/giffytasks/UserQuiz.kt
@@ -0,0 +1,10 @@
+package equipo1.giffytasks
+
+data class UserQuiz(var pregunta1: String ?= null, var pregunta2: String ?= null,
+                    var pregunta3: String ?= null, var pregunta4: String ?= null,
+                    var pregunta5: String ?= null, var pregunta6: String?= null,
+                    var pregunta7: String ?= null, var pregunta8: String ?= null,
+                    var pregunta9: String ?= null, var pregunta10: String ?= null,
+                    var pregunta11: String ?= null, var pregunta12: String ?= null,
+                    var pregunta13: String ?= null, var pregunta14: String ?= null,
+                    var pregunta15: String ?= null)
--- app/src/main/java/equipo1/giffytasks/question.kt
+++ app/src/main/java/equipo1/giffytasks/question.kt
@@ -3,24 +3,54 @@ package equipo1.giffytasks
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import equipo1.giffytasks.databinding.ActivityQuestionBinding
 
 class question : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityQuestionBinding
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_question)
+        binding = ActivityQuestionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button: Button = findViewById(R.id.button)
-        val button2: Button = findViewById(R.id.button2)
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
 
-        button.setOnClickListener{
+        binding.btnQuiz.setOnClickListener{
             var intent: Intent = Intent(this,quiz::class.java)
             startActivity(intent)
         }
 
-        button2.setOnClickListener{
-            var intent: Intent = Intent(this,NavigationActivity::class.java)
-            startActivity(intent)
+        binding.btnNoquiz.setOnClickListener{
+            fillQuiz()
+        }
+    }
+
+    private fun fillQuiz() {
+        val uid = auth.currentUser?.uid
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+        val userQuiz = UserQuiz("empty", "empty", "empty", "empty",
+            "empty", "empty", "empty", "empty", "empty",
+            "empty", "empty", "empty", "empty", "empty",
+            "empty")
+        if (uid != null) {
+            databaseReference.child("user").child(uid).child("quiz").setValue(userQuiz).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    var intent: Intent = Intent(this,NavigationActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "Fallo al registrar el quiz.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
