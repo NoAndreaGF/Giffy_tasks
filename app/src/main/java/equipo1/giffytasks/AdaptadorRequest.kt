dc44e63fbe4f1477866a6b06242f7f8c1ee10415
Paloma <paloma.arguello216561@potros.itson.edu.mx>
2022-05-17 01:38:08 -0500

    Solicitudes

--- app/src/main/java/equipo1/giffytasks/AdaptadorRequest.kt
+++ app/src/main/java/equipo1/giffytasks/AdaptadorRequest.kt
@@ -8,25 +8,36 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
 
-class AdaptadorRequest(private val usersList: ArrayList<User>, private val  picsArrayList: ArrayList<String>) : RecyclerView.Adapter<AdaptadorRequest.MyViewHolder>() {
 
-    public class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val imagen : ImageView = itemView.findViewById(R.id.iv_usuario_solicitud)
-        val nombre : TextView = itemView.findViewById(R.id.tv_nombre_solicitud)
+class AdaptadorRequest(
+    private val usersList: ArrayList<User>,
+    private val picsArrayList: ArrayList<String>
+) : RecyclerView.Adapter<AdaptadorRequest.MyViewHolder>() {
+    private lateinit var auth: FirebaseAuth
+
+    public class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imagen: ImageView = itemView.findViewById(R.id.iv_usuario_solicitud)
+        val nombre: TextView = itemView.findViewById(R.id.tv_nombre_solicitud)
+        val btnagrega: TextView = itemView.findViewById(R.id.btn_accept)
+        val btnrechaza: TextView = itemView.findViewById(R.id.btn_reject)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_request_view,
-            parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.activity_request_view,
+            parent, false
+        )
         return MyViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val usuario : User = usersList[position]
-        val uid : String = picsArrayList[position]
+        val usuario: User = usersList[position]
+        val uid: String = picsArrayList[position]
         holder.nombre.text = usuario.nombre
 
         var storageRef = FirebaseStorage.getInstance().getReference("image/" + uid)
@@ -45,12 +56,40 @@ class AdaptadorRequest(private val usersList: ArrayList<User>, private val  pics
         holder.nombre.setOnClickListener {
             onClickFriend(it, holder, uid)
         }
+        holder.btnagrega.setOnClickListener {
+            addFriend(uid)
+        }
+        holder.btnrechaza.setOnClickListener {
+           rejectFriend()
+        }
+    }
+
+    private fun rejectFriend() {
+        val uid2 = FirebaseAuth.getInstance().currentUser!!.uid
+        var databaseReference = FirebaseDatabase.getInstance().getReference("users")
+        databaseReference.child("user").child(uid2).child("solicutudes").child(uid).removeValue()
+    }
+
+    private fun addFriend(uid: String) {
+        val uid2 = FirebaseAuth.getInstance().currentUser!!.uid
+        var databaseReference = FirebaseDatabase.getInstance().getReference("users")
+        if (uid2 != null) {
+            databaseReference.child("user").child(uid2).child("amigos").push()
+                .setValue(uid).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                       // rejectFriend()
+                    } else {
+
+                    }
+                }
+
+        }
     }
 
     fun onClickFriend(v: View?, holder: MyViewHolder, uid: String) {
         val intent = Intent(v!!.context, wishlist::class.java)
-        intent.putExtra("nombre",  holder.nombre.text)
-        intent.putExtra("uid",  uid)
+        intent.putExtra("nombre", holder.nombre.text)
+        intent.putExtra("uid", uid)
         v!!.context.startActivity(intent)
     }
 
